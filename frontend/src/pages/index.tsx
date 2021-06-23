98596ca1900f4937acb25b60fcea53d18cfa7eb0
joshchen984 <joshchen984@gmail.com>
2021-06-22 17:27:31 -0700

    Create theme

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,23 @@
+import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    color: theme.palette.darkBlue,
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
   return (
-  <div></div>
+    <Grid container direction="column">
+      <Typography variant="h1" className={classes.title}>
+        Organize Your Grades
+      </Typography>
+    </Grid>
   );
 }
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,4 +1,37 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({});
+const blue = '#55A6D8';
+const orange = '#E99B26';
+const darkBlue = '#004E79';
+const brown = '#8E4F00';
+
+declare module '@material-ui/core/styles/createPalette' {
+  interface Palette {
+    blue?: string;
+    orange?: string;
+    darkBlue?: string;
+    brown?: string;
+  }
+  interface PaletteOptions {
+    blue?: string;
+    orange?: string;
+    darkBlue?: string;
+    brown?: string;
+  }
+}
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue,
+    },
+    secondary: {
+      main: orange,
+    },
+    blue,
+    orange,
+    darkBlue,
+    brown,
+  },
+});
 export default theme;
