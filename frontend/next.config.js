c05f00a192ba673d334bcdcb82b73fa6f8f6d0cd
joshchen984 <joshchen984@gmail.com>
2021-07-13 13:40:22 -0700

    Connect frontend and backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api',
+        destination: 'http://localhost:3001', // Proxy to Backend
+      },
+    ];
+  },
 };
