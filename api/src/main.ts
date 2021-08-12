90774f6841d2110e644335d1ed0098dd35c64367
joshchen984 <joshchen984@gmail.com>
2021-08-11 17:23:54 -0700

    Enable Cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  }
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3001);
 }
